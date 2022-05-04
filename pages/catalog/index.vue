cc95ada7dc10a725eb68f4afc6527433e58e76f5
Andrey Chursin <AndreyChursin@gmail.com>
2022-05-04 18:28:00 +0300

    add catalog

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col v-for="item in items" :key="item.id" cols="12" sm="8" md="6">
-      <span>{{item.name}}</span>
+    <v-col v-for="product in products" :key="product.id" cols="12" sm="3" md="4">
+      <v-card @click.prevent="openProduct(product)">
+        <v-card-title class="headline">{{product.name}}</v-card-title>
+        <v-card-text>
+          <span>Price: {{product.price}}</span>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -10,13 +15,31 @@
 export default {
   name: 'catalogPage',
   computed: {
-    items: (state) => [
+    products: (state) => [
       {
         id: 1,
         name: 'item 1',
-        price: 100
-      }
+        price: 100,
+        description: 'description item 1'
+      },
+      {
+        id: 2,
+        name: 'item 2',
+        price: 200,
+        description: 'description item 2'
+      },
+      {
+        id: 3,
+        name: 'item 3',
+        price: 3000,
+        description: 'description item 3'
+      },
     ]
+  },
+  methods: {
+    openProduct(product) {
+      this.$router.push('/catalog/product/' + product.id)
+    }
   }
 }
 </script>
---
+++ pages/catalog/product/_id.vue
@@ -0,0 +1,20 @@
+<template>
+   <v-row justify="center" align="center">
+    <v-col cols="12" sm="3" md="4">
+      <v-card>
+        <v-card-title class="headline">Product name</v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'productPage',
+  validate(contecst) {
+    console.log(contecst);
+    return true;
+    // /^\d+$/gi.test(params.id)
+  },
+}
+</script>
