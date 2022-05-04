8c6ec1e32b67df5cdfcb50ce5a95a1a8d53d4499
Andrey Chursin <AndreyChursin@gmail.com>
2022-05-04 18:10:32 +0300

    start

---
+++ pages/catalog/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col v-for="item in items" :key="item.id" cols="12" sm="8" md="6">
+      <span>{{item.name}}</span>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'catalogPage',
+  computed: {
+    items: (state) => [
+      {
+        id: 1,
+        name: 'item 1',
+        price: 100
+      }
+    ]
+  }
+}
+</script>
